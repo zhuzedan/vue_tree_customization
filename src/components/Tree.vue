84ffa02dba5979aae72d0ee7b798d54457382790
zhuzedan <1031155817@qq.com>
2024-04-18 17:11:30 +0800

    feat:tree和dialog写一起

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="white-body-view">
+    <el-button type="primary" @click="addNewRecord()">新增</el-button>
+
     <el-tree
         id="my-tree"
         ref="tree"
         class="tree-view structure-tree"
         :data="treeData"
         highlight-current
-        :default-expand-all="treeExpandAll"
+        :default-expand-all="true"
         :props="defaultProps"
         check-strictly
-        :node-key="treeNodeKey"
         :auto-expand-parent="false"
         :expand-on-click-node="false"
     >
@@ -34,35 +35,63 @@
         </div>
       </span>
     </el-tree>
+    <place-dialog ref="placeDialog" @addData="addData" @editData="editData"/>
   </div>
 </template>
 
 <script>
+import PlaceDialog from "@/components/PlaceDialog.vue";
+
 export default {
-  props: {
-    // 源数据
-    treeData: {
-      type: Array,
-      default: function () {
-        return []
-      }
-    },
-
-    // 树节点是否默认展开
-    treeExpandAll: {
-      type: Boolean,
-      default: true
-    },
-
-    // 树节点唯一标识
-    treeNodeKey: {
-      type: String,
-      default: ''
-    }
+  components: {
+    PlaceDialog
   },
-
   data() {
     return {
+      treeData: [
+        {
+          children: [
+            {
+              children: [],
+              name: '1楼',
+              desc: '这是教学楼1楼',
+              parentId: '1',
+              id: '2'
+            },
+            {
+              children: [],
+              name: '2楼',
+              desc: '这是教学楼1楼',
+              parentId: '1',
+              id: '3'
+            },
+            {
+              children: [],
+              name: '3楼',
+              desc: '这是教学楼3楼',
+              parentId: '1',
+              id: '4'
+            }
+          ],
+          name: '教学楼',
+          parentId: '',
+          id: '1'
+        },
+        {
+          children: [
+            {
+              children: [],
+              name: '办公1楼',
+              desc: '这是办公楼',
+              parentId: '5',
+              id: '6'
+            }
+          ],
+          name: '办公楼',
+          parentId: '',
+          id: '5'
+        }
+      ],
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -73,20 +102,41 @@ export default {
   },
 
   methods: {
+    // 添加新记录
+    addNewRecord() {
+      this.$refs.placeDialog.openDialog(false)
+    },
     // 添加新增按钮
     handleAdd(data) {
-      this.$emit('addItem', data)
+      this.$refs.placeDialog.openDialog(false, data.id)
     },
 
     // 点击删除按钮
     handleDelete(data) {
-      this.$emit('deleteItem', data)
+      this.$confirm('确定删除吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 删除树节点
+        this.treeDeleteItem(data)
+        // 提示
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     },
 
     // 点击编辑按钮
     handleEdit(data) {
       this.selectItem = data
-      this.$emit('editItem', JSON.parse(JSON.stringify(data)))
+      this.$refs.placeDialog.openDialog(true, data.parentId, data)
     },
 
     // ============== 组件内事件 结束=============
@@ -117,7 +167,19 @@ export default {
 
     handleMouseLeave(data) {
       this.$set(data, 'show', false)
-    }
+    },
+
+    // 新增表单数据
+    addData(data) {
+      // 新增树节点
+      this.treeAddItem(data)
+    },
+
+    // 修改表单数据
+    editData(data) {
+      // 修改树节点
+      this.treeEditItem(data)
+    },
   }
 }
 </script>
